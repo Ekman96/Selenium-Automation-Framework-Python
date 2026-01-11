cbe2992e413018f649a365e1dac689fa06faca55
Ekmanpreet Singh <ekmanpreet4@gmail.com>
2026-01-11 03:27:52 -0500

    Fix SessionnotcreatedException with CI-safe chrome options

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -4,12 +4,14 @@ from selenium.webdriver.chrome.options import Options
 def get_driver(headless: bool = False):
     options = Options()
 
+    # These options make Chrome stable on CI/Linux
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+
     if headless:
         options.add_argument("--headless=new")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
     return driver
