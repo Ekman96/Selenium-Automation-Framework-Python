765929452bd5592ec1f73423c0526d14887427fc
Ekmanpreet Singh <ekmanpreet4@gmail.com>
2026-01-11 03:34:25 -0500

    Fix Selenium CI using Chromium + matching chromedriver

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 def get_driver(headless: bool = False):
     options = Options()
 
-    # These options make Chrome stable on CI/Linux
+    # Chromium path on Ubuntu runners
+    options.binary_location = "/usr/bin/chromium-browser"
+
+    # CI-safe flags
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
@@ -13,5 +17,8 @@ def get_driver(headless: bool = False):
     if headless:
         options.add_argument("--headless=new")
 
-    driver = webdriver.Chrome(options=options)
+    # Chromedriver path on Ubuntu runners
+    service = Service("/usr/bin/chromedriver")
+
+    driver = webdriver.Chrome(service=service, options=options)
     return driver
